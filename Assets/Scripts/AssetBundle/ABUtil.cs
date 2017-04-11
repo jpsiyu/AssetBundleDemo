e195a4985157902c559f22af14765c2e22360e10
jpsiyu <jpsiyu@163.com>
2017-04-11 17:00:59 +0800

    add log view for checking

--- Assets/Scripts/AssetBundle/ABUtil.cs
+++ Assets/Scripts/AssetBundle/ABUtil.cs
@@ -8,8 +8,14 @@ public class ABUtil  {
 
     public static void Log(params string[] logs) {
         for (int i = 0; i < logs.Length; i++) {
-            Debug.Log(string.Format("<color={0}>ABFlow: {1}</color>", yellowCode, logs[i]));
+            string log = string.Format("<color={0}>ABFlow: {1}</color>", yellowCode, logs[i]);
+            Debug.Log(log);
+            LogToView(log);
         }
     }
 
+    public static void LogToView(string log) {
+        LogView logView = GameObject.FindObjectOfType<LogView>();
+        if (logView != null) logView.AddLog(log);
+    }
 }
---
+++ Assets/Scripts/View/LogView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LogView : MonoBehaviour {
+
+    private GameObject mLogText;
+    private GameObject mLogRoot;
+
+    void Awake() {
+        mLogText = transform.FindChild("Log").gameObject;
+        mLogRoot = transform.FindChild("Scroll View/Viewport/Content").gameObject;
+    }
+
+    public void AddLog(string logStr) {
+        GameObject aLog = GameObject.Instantiate(mLogText);
+        aLog.SetActive(true);
+        aLog.GetComponent<Text>().text = logStr;
+        aLog.transform.SetParent(mLogRoot.transform);
+        aLog.transform.localScale = Vector3.one;
+    }
+}
